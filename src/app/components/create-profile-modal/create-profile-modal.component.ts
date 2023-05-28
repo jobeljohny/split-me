3d344f9d40665f01371a3270fb79e21ac570d52e
jobel johny <jobeljohny@gmail.com>
2023-05-29 01:03:06 +0530

    migrated rest of the modals ...phew

--- src/app/components/create-profile-modal/create-profile-modal.component.ts
+++ src/app/components/create-profile-modal/create-profile-modal.component.ts
@@ -37,8 +37,4 @@ export class CreateProfileModalComponent {
       ],
     });
   }
-
-  onCancel() {
-    this.dialogRef.close();
-  }
 }
--- src/app/components/summary-qr-modal/summary-qr-modal.component.ts
+++ src/app/components/summary-qr-modal/summary-qr-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
-import { Modal } from 'bootstrap';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { SummaryExportService } from 'src/app/services/summary-export.service';
 
 @Component({
   selector: 'app-summary-qr-modal',
@@ -7,41 +8,20 @@ import { Modal } from 'bootstrap';
   styleUrls: ['./summary-qr-modal.component.scss'],
 })
 export class SummaryQrModalComponent {
-  @ViewChild('SummaryQR') Modal: any;
-  myModal: any;
-  @Input('link') QR: string = 'unknown';
   isLoading = true;
-  characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  link: string = 'unknown';
 
-  generateString(length: number) {
-    let result = ' ';
-    const charactersLength = this.characters.length;
-    for (let i = 0; i < length; i++) {
-      result += this.characters.charAt(
-        Math.floor(Math.random() * charactersLength)
-      );
-    }
-
-    return result;
-  }
-  constructor() {}
-
-  ngAfterViewInit() {
-    this.myModal = new Modal(this.Modal.nativeElement, {
-      backdrop: 'static',
-      keyboard: true,
+  constructor(
+    public dialogRef: MatDialogRef<SummaryQrModalComponent>,
+    private summaryExport: SummaryExportService
+  ) {
+    this.summaryExport.qrLink$.subscribe((link) => {
+      this.link = link;
     });
   }
-  showModal() {
-    this.myModal.show();
-  }
-  close() {
-    this.myModal.hide();
-    this.QR = 'unknown';
-    this.isLoading = true;
-  }
+
   onChangeURL(event: any) {
-    if (this.QR != 'unknown') {
+    if (this.link != 'unknown') {
       this.isLoading = false;
     }
   }
