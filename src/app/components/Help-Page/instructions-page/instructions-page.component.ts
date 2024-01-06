51582ed79f2eed5d1495fdbe593156d447b0f6c9
jobeljohny <jobeljohny@gmail.com>
2024-01-06 21:39:27 +0530

    implemented slider

--- src/app/components/Help-Page/instructions-page/instructions-page.component.ts
+++ src/app/components/Help-Page/instructions-page/instructions-page.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-instructions-page',
   templateUrl: './instructions-page.component.html',
-  styleUrls: ['./instructions-page.component.scss']
+  styleUrls: ['./instructions-page.component.scss'],
 })
 export class InstructionsPageComponent {
+  selectedItem: string = '';
+  radioButtons: number[] = [1, 2, 3, 4, 5];
+  selectedIndex: number = 0;
+  constructor() {
+    //  setInterval(()=>console.log(this.selectedItem),500)
+  }
 
+  onRadioButtonChanged(index: number) {
+    this.selectedIndex = index;
+  }
+  onButtonClick(index: number) {
+    this.selectedIndex = index;
+  }
 }
