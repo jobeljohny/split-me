c5c78a0fa15be81fa81b753dbc064e5d42737d43
jobeljohny <jobeljohny@gmail.com>
2024-01-22 23:25:22 +0530

    migrated animations

--- src/app/components/food-palettes-box/food-palettes-box.component.ts
+++ src/app/components/food-palettes-box/food-palettes-box.component.ts
@@ -1,68 +1,21 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { buttonAnimation, cardAnimation } from 'src/app/classes/animations';
 import { FoodItem } from 'src/app/classes/food-item';
+import { IBillEntry } from 'src/app/classes/interfaces';
 import { FoodPaletteService } from 'src/app/services/food-palette.service';
 import { KeyBindingService } from 'src/app/services/keybinding.service';
-import { ImportModalComponent } from '../import-modal/import-modal.component';
 import { HelpDialogComponent } from '../Help-Page/help-dialog/help-dialog.component';
 import { pages } from '../Help-Page/help-dialog/help-page-utils';
-import { IBillEntry } from 'src/app/classes/interfaces';
-import {
-  animate,
-  keyframes,
-  query,
-  stagger,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import { ImportModalComponent } from '../import-modal/import-modal.component';
 @Component({
   selector: 'app-food-palettes-box',
   templateUrl: './food-palettes-box.component.html',
   styleUrls: ['./food-palettes-box.component.scss'],
-  animations: [
-    trigger('cardAnimation', [
-      transition('* => void',[]),
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), { optional: true }),
-        query(
-          ':enter',
-          stagger('200ms', [
-            animate(
-              '.4s ease-in',
-              keyframes([
-                style({ opacity: 0, transform: 'translateY(-50%)', offset: 0 }),
-                style({
-                  opacity: 0.5,
-                  transform: 'translateY(-10px) scale(1.1)',
-                  offset: 0.3,
-                }),
-                style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
-              ])
-            ),
-          ]),
-          { optional: true }
-        ),
-        query(
-          ':leave',
-          stagger('200ms', [
-            animate(
-              '0.4s ease-out',
-              keyframes([
-                style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
-                style({ opacity: 0.5, transform: 'scale(.5)', offset: 0.3 }),
-                style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
-              ])
-            ),
-          ]),
-          { optional: true }
-        ),
-      ]),
-    ]),
-  ],
+  animations: [cardAnimation, buttonAnimation],
 })
 export class FoodPalettesBoxComponent {
+  buttonState: number = 0;
   @ViewChild('itemWrapper') myScrollContainer!: ElementRef;
 
   constructor(
@@ -73,11 +26,12 @@ export class FoodPalettesBoxComponent {
     this.keyBinding.handleAltF(this.onAddFoodPalette.bind(this));
     //TODO remove
     setTimeout(() => {
-   //   this.onImportBill();
+      this.onImportBill();
     }, 200);
   }
 
   onAddFoodPalette() {
+    this.buttonState++;
     this.foodPalette.add();
 
     //TODO create a smoother transition for this or discard scroll effect
@@ -105,6 +59,7 @@ export class FoodPalettesBoxComponent {
 
   removeFoodTile(item: FoodItem) {
     this.foodPalette.remove(item);
+    this.buttonState--;
   }
 
   openDialog() {
